a866c26cd3ee2d66549c34bb0914d4ffcc072c70
Mikhail Stepanov <Mikhail.Stepanov@returnonintelligence.com>
2016-07-25 12:11:35 +0300

    typo missed

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/', function(req, response, next) {
         Info.find((err,data)=>{
             if(err)console.log(err);
             else {
+                var showArray = {};
                 var letsSort = data;
                 var newArray = [];
 
